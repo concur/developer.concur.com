2f403300de669c53bbfbe96d80f09f943ad4d725
Howard Dierking <howarddierking@gmail.com>
2015-04-02 19:02:01 -0700

    added logging to see what the aws config file will look like

--- build/aws_cli_config.sh
+++ build/aws_cli_config.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+mkdir ~/.aws
 echo [default]\\noutput = text\\nregion = us-west-2\\naws_access_key_id = $PORTAL_AWS_ACCESS_KEY_ID\\naws_secret_access_key = $PORTAL_AWS_SECRET_ACCESS_KEY > ~/.aws/config
+cat ~/.aws/config
