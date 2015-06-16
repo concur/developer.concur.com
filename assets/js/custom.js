d50a65d3880d503fbbcab2469347af5bcaae9ea8
nicklathe <nicklathe@icloud.com>
2015-06-16 15:11:39 -0700

    Working on pulling in concur blog and parsing xml

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,9 @@
 /* Write here your custom javascript codes */
+
+$(document).ready(function(){
+  console.log('Ready');
+  $.get('https://www.concur.com/blog/category/en-us/feed', function(data){
+    var blogs = $(data).find('item');
+    console.log(blogs);
+  })
+});
