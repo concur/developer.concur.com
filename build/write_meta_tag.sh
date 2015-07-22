23bc84ea032814a441cde558e8c5472def787cb6
nicklathe <nicklathe@icloud.com>
2015-07-22 10:34:20 -0700

    fixed faulty logic. now writes noindex meta tag to yml file on any branch except livesite

--- build/write_meta_tag.sh
+++ build/write_meta_tag.sh
@@ -4,6 +4,9 @@ BRANCHNAME=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 
 if [ ${BRANCHNAME} == "livesite" ]
 then
+  echo tag: > _data/meta_tag.yml
+  cat _data/meta_tag.yml
+else
   echo tag: '<META name="robots" content="noindex">' > _data/meta_tag.yml
   cat _data/meta_tag.yml
 fi
