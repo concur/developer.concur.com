91c8ba2f5ac61400cd742ffac9a96a7d9f461b8e
merittrompeter <mktrompeter@gmail.com>
2016-03-23 15:56:33 -0700

    clean

--- swagger-ui/swagger-ui.js
+++ swagger-ui/swagger-ui.js
@@ -4,9 +4,6 @@
  * @link http://swagger.io
  * @license Apache-2.0
  */
-
- console.log(replacementURL);
- 
 (function(){this["Handlebars"] = this["Handlebars"] || {};
 this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};
 this["Handlebars"]["templates"]["apikey_button_view"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
