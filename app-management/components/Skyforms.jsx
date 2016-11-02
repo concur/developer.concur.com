eb893d757d9cec535624e5d891f08f93f186512a
Brian Hague <brian.hague@concur.com>
2016-11-02 15:46:39 -0700

    refactored skyforms to use a wrapper component

--- app-management/components/Skyforms.jsx
+++ app-management/components/Skyforms.jsx
@@ -5,14 +5,30 @@
 import React, { PropTypes } from 'react';
 import Select from 'react-select';
 
-export const renderInput = ({ input, name, type, label, meta: { touched, error } }) => (
+// Wrap each render component with a label and Skyform classes needed for errors
+const FieldWrapper = ({ name, label, touched, error, classNames, children }) => (
   <div>
     <label className="label" htmlFor={name}>{label}</label>
-    <div className={touched && error ? 'input state-error' : 'input'}>
-      <input {...input} id={name} type={type} />
+    <div className={touched && error ? `${classNames} state-error` : classNames}>
+      {children}
     </div>
-    {touched && error && <em htmlFor={name} className="invalid">{error}</em>}
+    {touched && error && <em htmlFor={name} className="invalid">{error.join(', ')}</em>}
   </div>
+);
+
+FieldWrapper.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  touched: PropTypes.bool.isRequired,
+  error: PropTypes.array,
+  classNames: PropTypes.string,
+  children: PropTypes.any.isRequired,
+};
+
+export const renderInput = ({ input, name, type, label, meta: { touched, error } }) => (
+  <FieldWrapper name={name} label={label} touched={touched} error={error} classNames="input">
+    <input {...input} id={name} type={type} />
+  </FieldWrapper>
 );
 
 renderInput.propTypes = {
@@ -23,6 +39,59 @@ renderInput.propTypes = {
   meta: PropTypes.object.isRequired,
 };
 
+export const renderTextarea = ({ input, name, label, meta: { touched, error } }) => (
+  <FieldWrapper name={name} label={label} touched={touched} error={error} classNames="textarea">
+    <textarea {...input} id={name} />
+  </FieldWrapper>
+);
+
+renderTextarea.propTypes = {
+  input: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  meta: PropTypes.object.isRequired,
+};
+
+export const renderSelect = ({ input, name, label, options, meta: { touched, error } }) => (
+  <FieldWrapper name={name} label={label} touched={touched} error={error}>
+    <Select
+      {...input}
+      id={name}
+      options={options}
+      onBlur={() => input.onBlur(input.value)}
+    />
+  </FieldWrapper>
+);
+
+renderSelect.propTypes = {
+  input: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  options: PropTypes.array.isRequired,
+  meta: PropTypes.object.isRequired,
+};
+
+export const renderMultiSelect = ({ input, name, label, options, meta: { touched, error } }) => (
+  <FieldWrapper name={name} label={label} touched={touched} error={error}>
+    <Select
+      {...input}
+      id={name}
+      multi
+      options={options}
+      onBlur={() => input.onBlur(input.value)}
+    />
+  </FieldWrapper>
+);
+
+renderMultiSelect.propTypes = {
+  input: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  options: PropTypes.array.isRequired,
+  meta: PropTypes.object.isRequired,
+};
+
+// Due to the checkbox being wrapped by a label, this component doesn't use FieldWrapper
 export const renderCheckbox = ({ input, name, type, meta: { touched, error }, children }) => (
   <div>
     <label className={touched && error ? 'checkbox state-error' : 'checkbox'} htmlFor={name}>
@@ -40,68 +109,4 @@ renderCheckbox.propTypes = {
   type: PropTypes.string.isRequired,
   meta: PropTypes.object.isRequired,
   children: PropTypes.any,
-};
-
-export const renderTextarea = ({ input, name, label, meta: { touched, error } }) => (
-  <div>
-    <label className="label" htmlFor={name}>{label}</label>
-    <div className={touched && error ? 'textarea state-error' : 'textarea'}>
-      <textarea {...input} id={name} />
-    </div>
-    {touched && error && <em htmlFor={name} className="invalid">{error}</em>}
-  </div>
-);
-
-renderTextarea.propTypes = {
-  input: PropTypes.object.isRequired,
-  name: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  meta: PropTypes.object.isRequired,
-};
-
-export const renderSelect = ({ input, name, label, options, meta: { touched, error } }) => (
-  <div>
-    <label className="label" htmlFor={name}>{label}</label>
-    <div className={touched && error ? 'state-error' : ''}>
-      <Select
-        {...input}
-        id={name}
-        options={options}
-        onBlur={() => input.onBlur(input.value)}
-      />
-    </div>
-    {touched && error && <em htmlFor={name} className="invalid">{error}</em>}
-  </div>
-);
-
-renderSelect.propTypes = {
-  input: PropTypes.object.isRequired,
-  name: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  options: PropTypes.array.isRequired,
-  meta: PropTypes.object.isRequired,
-};
-
-export const renderMultiSelect = ({ input, name, label, options, meta: { touched, error } }) => (
-  <div>
-    <label className="label" htmlFor={name}>{label}</label>
-    <div className={touched && error ? 'state-error' : ''}>
-      <Select
-        {...input}
-        id={name}
-        multi
-        options={options}
-        onBlur={() => input.onBlur(input.value)}
-      />
-    </div>
-    {touched && error && <em htmlFor={name} className="invalid">{error}</em>}
-  </div>
-);
-
-renderMultiSelect.propTypes = {
-  input: PropTypes.object.isRequired,
-  name: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  options: PropTypes.array.isRequired,
-  meta: PropTypes.object.isRequired,
 };
