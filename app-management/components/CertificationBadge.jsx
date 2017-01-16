d6bd09e7ad3329967ca145401c30d0586699baf1
Brian Hague <brian.hague@concur.com>
2017-01-16 08:41:56 -0800

    added colors to certification badge

--- app-management/components/CertificationBadge.jsx
+++ app-management/components/CertificationBadge.jsx
@@ -2,11 +2,11 @@ import React, { PropTypes } from 'react';
 
 const CertificationBadge = ({ certified }) => (
   certified ? (
-    <small>
+    <small className="certification-badge certified">
       <i className="fa fa-check-circle" aria-hidden="true" /> CERTIFIED
     </small>
   ) : (
-    <small>
+    <small className="certification-badge not-certified">
       <i className="fa fa-times-circle" aria-hidden="true" /> NOT CERTIFIED
     </small>
   )
