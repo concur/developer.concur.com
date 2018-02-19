88ee3d3e9540f03341baed086b1fd28bcb20e356
Brian Hague <brian.hague@concur.com>
2018-02-19 13:54:49 -0800

    Fix prop-types deprecation warning

--- app-management/components/NewAppPage/NewAppPage.jsx
+++ app-management/components/NewAppPage/NewAppPage.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import NewAppForm from './NewAppForm';
 import LoadingSpinner from '../LoadingSpinner';
 import ErrorAlert from '../ErrorAlert';
