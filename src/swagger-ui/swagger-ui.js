c750882a1d6b0b8548656e65e591b089db0ed2d8
Charlie Owen <charlie.owen01@sap.com>
2019-03-29 15:28:46 -0700

    Tiny whitespace to perhaps bump the deployment.

--- src/swagger-ui/swagger-ui.js
+++ src/swagger-ui/swagger-ui.js
@@ -415,7 +415,7 @@ this["Handlebars"]["templates"]["operation"] = Handlebars.template({"1":function
   return "          <h4 data-sw-translate>Parameters</h4>\n          <table class='fullwidth'>\n          <thead>\n            <tr>\n            <th style=\"width: 100px; max-width: 100px\" data-sw-translate>Parameter</th>\n            <th style=\"width: 310px; max-width: 310px\" data-sw-translate>Value</th>\n            <th style=\"width: 200px; max-width: 200px\" data-sw-translate>Description</th>\n            <th style=\"width: 100px; max-width: 100px\" data-sw-translate>Parameter Type</th>\n            <th style=\"width: 220px; max-width: 230px\" data-sw-translate>Data Type</th>\n            </tr>\n          </thead>\n          <tbody class=\"operation-params\">\n\n          </tbody>\n          </table>\n";
   },"18":function(depth0,helpers,partials,data) {
     var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-    return "          <h4><span data-sw-translate>Response Class</span> (<span data-sw-translate>Status</span> "
+    return "           <h4><span data-sw-translate>Response Class</span> (<span data-sw-translate>Status</span> "
       + escapeExpression(((helper = (helper = helpers.successCode || (depth0 != null ? depth0.successCode : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"successCode","hash":{},"data":data}) : helper)))
       + ")</h4>\n          <p><span class=\"model-signature\" /></p>\n          <br/>\n          <div class=\"response-content-type\" />\n";
   },"20":function(depth0,helpers,partials,data) {
